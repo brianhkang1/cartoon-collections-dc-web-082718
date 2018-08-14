7c5360ad876eeb0e3130641304e3069a96b9cfb6
Brian Kang <brianhkang1@gmail.com>
2018-08-14 17:38:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def long_planeteer_calls(array)
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
